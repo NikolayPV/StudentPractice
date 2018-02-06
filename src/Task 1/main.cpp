d4905585063766c23d8d3b6a43d8d2c2de3873d8
Nikolay <greedisgoodthe@gmai.com>
2018-02-06 21:57:35 +0200

    Answer for Task1

--- src/Task 1/main.cpp
+++ src/Task 1/main.cpp
@@ -1,7 +1,53 @@
 #include <iostream>
+#include <cstdlib>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
 
-int main(int argc, char** argv) {
+char result[11];
+int i = 0;
+
+void write_result()
+{
+		i = i-1;
+		cout << "This namber in Bin = ";
+		for(i; i >= 0; i--)
+		{
+			cout << result[i];
+		}
+		cout << "\n \n";
+}	
+
+int main() 
+{
+	int giveDecNam;
+	int showBin;
+	int changes;
+	int oneOrZero;
+	
+	for(int j = 0; j < 10; j++)
+	{
+		giveDecNam = 1 + rand() % 512;
+		cout << "Dec namber = " << giveDecNam << endl;
+			do
+			{
+				changes = giveDecNam / 2;
+					oneOrZero = giveDecNam - changes*2;
+				if ( 0 == oneOrZero)
+				{
+					result[i] = '0';
+				}
+				else
+				{
+					result[i] = '1';
+				}
+				giveDecNam = changes;
+				i++;
+			}
+		while(giveDecNam != 0);
+		
+		write_result();	
+	}
+	
 	return 0;
 }
+
