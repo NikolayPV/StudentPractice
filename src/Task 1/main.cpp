f0c3d312c068b7030f009ac8fd31b3e635b696ed
Nikolay <greedisgoodthe@gmai.com>
2018-02-08 14:41:38 +0200

    fixed  bugs in Task 1 - the announcement of variables outside of cycles - the variable looper in function numberAsBinary was renamed - constant MAX_SIZE_BIN_NAMB in function main was renamed - aded the new constant in function main. - added the obvious transformation of variables type. - changed while to for - changed the type of work with array

--- src/Task 1/main.cpp
+++ src/Task 1/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdint.h>
 #include <cstdlib>
 #include <cmath>
 #include <cstring>
@@ -6,51 +7,61 @@
 using namespace std;
 
 //-----------------------------------------------------------------------------------------
-void numberAsBinary( unsigned int number, char *_result, int looper);
+void numberAsBinary( unsigned int number, char *_result, int indexLocationInMass);
 
 //-----------------------------------------------------------------------------------------
 int main() 
 {
-	const int MAX_SIZE_BIN_NAMB = 10;
-	char result[MAX_SIZE_BIN_NAMB];
-	unsigned int randDecNamb;
+	const int MAX_BIN_NUMBER_LENGTH = 10;
+	const int QUANTITY_DEC_DIGITS = 10;
 	
-	double counterBitInDec;
-
-	for(int j = 0; j < 10; ++j)
+	char result[MAX_BIN_NUMBER_LENGTH];
+	
+	unsigned int randomDecimalNamber;
+	uint16_t counterBitInDec;
+	
+	int cleanMass;
+	
+	for(int j = 0; j < QUANTITY_DEC_DIGITS; ++j)
 	{
-		randDecNamb = 1 + rand() % 512;
-		counterBitInDec = log2(randDecNamb);
+		randomDecimalNamber = 1 + rand() % 512;
 		
-		numberAsBinary(randDecNamb, result, counterBitInDec);
+		counterBitInDec = static_cast<uint16_t>(log2(randomDecimalNamber) + 1);
+		result[counterBitInDec] = '\0';
+		
+		numberAsBinary(randomDecimalNamber, result, counterBitInDec);
 		
 		cout << "This namber in Bin = " << result << endl << endl;
 		
-		memset(result, 0, MAX_SIZE_BIN_NAMB*sizeof(int)); 
+		cleanMass = MAX_BIN_NUMBER_LENGTH*sizeof(int);
+		memset(result, 0, cleanMass); 
 	}
 	return 0;
 }
 
 //-----------------------------------------------------------------------------------------
-void numberAsBinary(unsigned int number, char *_result, int looper)
+void numberAsBinary(unsigned int number, char *result, int indexLocationInMass)
 {
-		cout << "Dec namber = " << number << endl;
-		 
-		while(looper >= 0)
-		{	
-			int changes = number / 2;
-			int oneOrZero = number - changes*2;
-			if ( 0 == oneOrZero)	
-			{
-				*(_result + looper) = '0';
-			}
-			else
-			{
-				*(_result + looper) = '1';
-			}
-			number = changes;
-			--looper;
+	cout << "Dec namber = " << number << endl;
+	int changes;
+	int oneOrZero;
+	
+	--indexLocationInMass;
+	
+	for( indexLocationInMass; indexLocationInMass >= 0; --indexLocationInMass)
+	{	
+		changes = number / 2;
+		oneOrZero = number - changes*2;
+		if ( 0 == oneOrZero)	
+		{
+			result[indexLocationInMass] = '0';
 		}
+		else
+		{
+			result[indexLocationInMass] = '1';
+		}
+		number = changes;
+	}
 
 }
 
