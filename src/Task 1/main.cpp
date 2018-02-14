4eee50b590ecb7ce286b3de426821d729d57e423
Nikolay <greedisgoodthe@gmai.com>
2018-02-14 15:15:16 +0200

    code optimization

--- src/Task 1/main.cpp
+++ src/Task 1/main.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
-#include <stdint.h>
-#include <cstdlib>
-#include <cmath>
-#include <cstring>
-#include <bitset>
+#include <stdio.h>
 
 using namespace std;
 
 //-----------------------------------------------------------------------------------------
-void numberAsBinary( unsigned int number, char *_result);
+void numberAsBinary( unsigned int number, char* result);
 
 //-----------------------------------------------------------------------------------------
 int main() 
@@ -16,45 +12,35 @@ int main()
 	const int MAX_BIN_NUMBER_LENGTH = 10;
 	const int QUANTITY_DEC_DIGITS = 10;
 	unsigned int randomDecimalNamber;
-	int cleaningArray;
-	
-	char *FindFirstUsed1;
-	char result[MAX_BIN_NUMBER_LENGTH];
+
+	char* result;
+	result = new char[MAX_BIN_NUMBER_LENGTH];
 
 	for(int j = 0; j < QUANTITY_DEC_DIGITS; ++j)
 	{
-		randomDecimalNamber = 1 + rand() % 512;
-		
+		cin >> randomDecimalNamber;
 		numberAsBinary(randomDecimalNamber, result);
-		result[MAX_BIN_NUMBER_LENGTH] = '\0';
-		
-		FindFirstUsed1 = strchr(result, '1');
-		cout << "This number in Bin = " << FindFirstUsed1 << endl;
-
-		cleaningArray = MAX_BIN_NUMBER_LENGTH*sizeof(int);
-		memset(result, 0, cleaningArray); 
+		printf("Result = %s \n", result);
 	}
 	return 0;
 }
 
 //-----------------------------------------------------------------------------------------
-void numberAsBinary(unsigned int number, char *result)
+void numberAsBinary(unsigned int number, char* result)
 {
-	int mask = 512;
-	
-	cout << "Dec number = " << number << endl;
-	for ( int i = 0; i <= 9; ++i )
+	int bitMask = 512;
+		
+	if(0 != result && number <= bitMask)
 	{
-		if ((number & mask) > 0)
+		while (bitMask > 0)
 		{
-			result[i] = '1';
+			if(number >= bitMask)
+			{
+				*(result++) = (number & bitMask) > 0 ? '1' : '0';
+			}	
+			bitMask >>= 1;
 		}
-		else
-		{
-			result[i] = '0';
-		}
-		mask = (mask >> 1);
-	}
-
+		*result = '\0';
+	}		
 }
-
+	
