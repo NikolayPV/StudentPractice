d06c7c1f0c1be0b30967d881b795b3316d01164c
Nikolay <greedisgoodthe@gmai.com>
2018-02-15 11:11:18 +0200

    Fixed last bugs

--- src/Task 1/main.cpp
+++ src/Task 1/main.cpp
@@ -1,46 +1,47 @@
 #include <iostream>
 #include <stdio.h>
+#include <climits>
 
 using namespace std;
-
 //-----------------------------------------------------------------------------------------
-void numberAsBinary( unsigned int number, char* result);
-
+void numberAsBinary( const unsigned int number, char* result);
 //-----------------------------------------------------------------------------------------
+typedef unsigned int UInt;
+static const UInt MAX_NUMBER = 512u;
+
 int main() 
 {
 	const int MAX_BIN_NUMBER_LENGTH = 10;
-	const int QUANTITY_DEC_DIGITS = 10;
 	unsigned int randomDecimalNamber;
 
 	char* result;
-	result = new char[MAX_BIN_NUMBER_LENGTH];
+	result = new char[sizeof( unsigned int) * CHAR_BIT];
 
-	for(int j = 0; j < QUANTITY_DEC_DIGITS; ++j)
+	for(int j = 0; j < MAX_BIN_NUMBER_LENGTH; ++j)
 	{
 		cin >> randomDecimalNamber;
 		numberAsBinary(randomDecimalNamber, result);
-		printf("Result = %s \n", result);
 	}
+	delete [] result;
 	return 0;
 }
 
 //-----------------------------------------------------------------------------------------
-void numberAsBinary(unsigned int number, char* result)
+void numberAsBinary( const unsigned int number, char* _result)
 {
-	int bitMask = 512;
+	UInt bitMask = MAX_NUMBER;
 		
-	if(0 != result && number <= bitMask)
+	if(0 != _result && number <= bitMask)
 	{
 		while (bitMask > 0)
 		{
 			if(number >= bitMask)
 			{
-				*(result++) = (number & bitMask) > 0 ? '1' : '0';
+				*(_result++) = (number & bitMask) > 0 ? '1' : '0';
 			}	
 			bitMask >>= 1;
 		}
-		*result = '\0';
+		*_result = '\0';
 	}		
 }
 	
