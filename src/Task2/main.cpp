1610678c0971be95101295288289cf6c55336674
Serhii Ratz <ratz@live.ru>
2018-02-15 11:30:14 +0100

    Task 2 for student practice

---
+++ src/Task2/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+/*******************************************************************
+* 
+*   Note this task is loaded from 
+*   https://www.hackerrank.com/challenges/insertionsort1/problem
+*
+********************************************************************/ 
+
+using namespace std;
+
+void insertionSort1(int n, vector <int> arr) {
+    // Complete this function
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int arr_i = 0; arr_i < n; arr_i++){
+       cin >> arr[arr_i];
+    }
+    insertionSort1(n, arr);
+    return 0;
+}
+
