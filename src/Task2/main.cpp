98143efb0c199ed7e2cffad368778c2e573949a5
Nikolay <greedisgoodthe@gmai.com>
2018-02-16 13:12:47 +0200

    First model of answer - realization algorithm

--- src/Task2/main.cpp
+++ src/Task2/main.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <vector>
 
 /*******************************************************************
 * 
@@ -9,18 +10,36 @@
 
 using namespace std;
 
-void insertionSort1(int n, vector <int> arr) {
-    // Complete this function
+void insertionSort1(int size, vector <int> arr);
+
+int main() 
+{
+	unsigned int ARRAY_SIZE;
+   cin >> ARRAY_SIZE;
+   vector<int> array(ARRAY_SIZE);
+   for(int i = 0; i < ARRAY_SIZE; ++i)
+	{
+		cin >> array[i];
+   }
+   insertionSort1(ARRAY_SIZE, array);
+//   for(int j = 0; j < ARRAY_SIZE; ++j) // Не понимаю почему он не выводит здесь.
+//		cout << array[j] << ' ';
+   return 0;
 }
 
-int main() {
-    int n;
-    cin >> n;
-    vector<int> arr(n);
-    for(int arr_i = 0; arr_i < n; arr_i++){
-       cin >> arr[arr_i];
-    }
-    insertionSort1(n, arr);
-    return 0;
+void insertionSort1(int vectorSize, vector <int> arr) 
+{
+	for (int i = 0; i < vectorSize; ++i)
+	{
+		int currentElemet = i;
+		while (currentElemet > 0 && arr[currentElemet - 1] > arr[currentElemet]) 
+		{
+			swap(arr[currentElemet], arr[currentElemet - 1]);
+			--currentElemet;
+		}
+		
+	} 
+//	for(int j = 0; j < vectorSize; ++j) 	// Если доваить вывод в этом месте выводится отсортированный массив. 
+//		cout << arr[j] << ' ';
 }
 
