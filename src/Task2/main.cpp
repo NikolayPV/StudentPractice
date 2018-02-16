842358493e652c053acee88054279c72d7338d50
Nikolay <greedisgoodthe@gmai.com>
2018-02-16 16:29:44 +0200

    chenged realization added passing by reference the answer display  step by step

--- src/Task2/main.cpp
+++ src/Task2/main.cpp
@@ -10,7 +10,7 @@
 
 using namespace std;
 
-void insertionSort1(int size, vector <int> arr);
+void insertionSort1(int size, vector <int> &arr);
 
 int main() 
 {
@@ -22,24 +22,23 @@ int main()
 		cin >> array[i];
    }
    insertionSort1(ARRAY_SIZE, array);
-//   for(int j = 0; j < ARRAY_SIZE; ++j) // Не понимаю почему он не выводит здесь.
-//		cout << array[j] << ' ';
+   for(int j = 0; j < ARRAY_SIZE; ++j) 
+		cout << array[j] << ' ';
    return 0;
 }
 
-void insertionSort1(int vectorSize, vector <int> arr) 
+void insertionSort1(int vectorSize, vector <int> &arr) 
 {
-	for (int i = 0; i < vectorSize; ++i)
+	int cutedElement = arr.back();
+	int currentElemet = vectorSize - 2;
+	while(cutedElement <= arr.at(currentElemet))
 	{
-		int currentElemet = i;
-		while (currentElemet > 0 && arr[currentElemet - 1] > arr[currentElemet]) 
-		{
-			swap(arr[currentElemet], arr[currentElemet - 1]);
-			--currentElemet;
-		}
-		
-	} 
-//	for(int j = 0; j < vectorSize; ++j) 	// Если доваить вывод в этом месте выводится отсортированный массив. 
-//		cout << arr[j] << ' ';
+		arr[currentElemet + 1] = arr[currentElemet];
+		--currentElemet;
+		for(int j = 0; j < vectorSize; ++j) 	
+		cout << arr[j] << ' ';
+		cout << endl;
+	}
+	arr[currentElemet + 1] = cutedElement;
 }
 
